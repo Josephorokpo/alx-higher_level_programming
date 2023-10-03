56073ff65d075d9d979dd72359e9f6da94fff517
josephorokpo <josephorokpo@gmail.com>
2023-10-03 06:23:19 -0700

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,4 +12,4 @@ elif last_digit == 0:
 else:
     status = "and is less than 6 and not 0"
 
-print(f"Last digit of {number} is {last_digit} and is {status}")
+print(f"Last digit of {number} is {last_digit} {status}")
