f8a1c2ea5167f6c047fc67328bffc04f249a2734
josephorokpo <josephorokpo@gmail.com>
2023-10-11 07:02:16 -0700

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """Replaces all occurrences of an element by another in a new list."""
+    new_list = [replace if item == search else item for item in my_list]
+    return (new_list)
