0f863bf9a9555985c9c1eb399a7623b530aefed3
josephorokpo <josephorokpo@gmail.com>
2023-11-07 13:42:41 -0800

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+A class Student that defines a student.
+"""
+
+
+class Student:
+    """
+    Defines a student with attributes and methods.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+
+    Methods:
+        to_json(self): Retrieves a dictionary representation
+        of a Student instance.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with the given attributes.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary with the student's attributes.
+        """
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
