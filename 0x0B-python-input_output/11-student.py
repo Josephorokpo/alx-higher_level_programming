2641c34258f263573b02d8fc302c3ea96167a4ad
josephorokpo <josephorokpo@gmail.com>
2023-11-07 13:58:49 -0800

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+A class Student that defines a student.
+"""
+
+
+class Student:
+    """
+    Defines a student by first name, last name, and age.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+
+    Methods:
+        to_json(self, attrs=None): Retrieves a dictionary representation
+        of a Student instance.
+        reload_from_json(self, json): Replaces all attributes of the
+        Student instance from a dictionary.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with the given attributes.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list, optional): A list of attribute names to include
+            in the dictionary.
+                If None, all attributes are retrieved.
+
+        Returns:
+            dict: A dictionary with the student's attributes.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance from a dictionary.
+
+        Args:
+            json (dict): A dictionary where keys represent public
+            attribute names and values
+                represent the values of the public attributes.
+
+        Returns:
+            None
+        """
+        self.__dict__.update(json)
