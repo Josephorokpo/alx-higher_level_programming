069fe6ad950b172530b92c70cfc81ec1fe36926a
josephorokpo <josephorokpo@gmail.com>
2023-11-12 13:06:09 -0800

    test_rectangle.py with the added update method

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,8 +9,8 @@ class TestRectangleUpdateMethod(unittest.TestCase):
 
     def test_update_method_no_args(self):
         """
-        Test if update method does not modify attributes when no
-        arguments are provided.
+        Test if update method does not modify attributes
+        when no arguments are provided.
         """
         rectangle = Rectangle(10, 20, 5, 8, 1)
         rectangle.update()
@@ -23,8 +23,8 @@ class TestRectangleUpdateMethod(unittest.TestCase):
 
     def test_update_method_with_args(self):
         """
-        Test if update method correctly updates attributes with
-        provided arguments.
+        Test if update method correctly updates attributes
+        with provided arguments.
         """
         rectangle = Rectangle(10, 20, 5, 8, 1)
         rectangle.update(2, 15, 25, 3, 4)
@@ -51,11 +51,25 @@ class TestRectangleUpdateMethod(unittest.TestCase):
 
     def test_update_method_extra_args(self):
         """
-        Test if update method ignores extra arguments beyond
-        the fifth argument.
+        Test if update method ignores extra arguments
+        beyond the fifth argument.
         """
         rectangle = Rectangle(10, 20, 5, 8, 1)
         rectangle.update(2, 15, 25, 3, 4, 99, "extra")
+
+        self.assertEqual(rectangle.id, 2)
+        self.assertEqual(rectangle.width, 15)
+        self.assertEqual(rectangle.height, 25)
+        self.assertEqual(rectangle.x, 3)
+        self.assertEqual(rectangle.y, 4)
+
+    def test_update_method_keyword_args(self):
+        """
+        Test if update method correctly updates
+        attributes with keyword arguments.
+        """
+        rectangle = Rectangle(10, 20, 5, 8, 1)
+        rectangle.update(id=2, width=15, height=25, x=3, y=4)
 
         self.assertEqual(rectangle.id, 2)
         self.assertEqual(rectangle.width, 15)
