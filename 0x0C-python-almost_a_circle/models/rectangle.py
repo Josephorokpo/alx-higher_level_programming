2482b72c689db59cd468a53a37c6032ba20e7b4a
josephorokpo <josephorokpo@gmail.com>
2023-11-13 00:43:46 -0800

    rectangle.py with the added update method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,13 +105,18 @@ class Rectangle(Base):
         """
         Update attributes with the provided arguments.
 
-        :param args: Positional arguments in \
-                the order (id, width, height, x, y).
-        :param kwargs: Keyword arguments for \
-                attributes (id, width, height, x, y).
+        :param args: Positional arguments in the order \
+                (id, width, height, x, y).
+        :param kwargs: Keyword arguments for attributes \
+                (id, width, height, x, y).
         """
         if args:
-            self.id, self.width, self.height, self.x, self.y = args[:5]
+            # Update id, width, height, x, y if provided
+            self.id = args[0] if len(args) > 0 else self.id
+            self.width = args[1] if len(args) > 1 else self.width
+            self.height = args[2] if len(args) > 2 else self.height
+            self.x = args[3] if len(args) > 3 else self.x
+            self.y = args[4] if len(args) > 4 else self.y
 
         # Handle keyword arguments
         for key, value in kwargs.items():
