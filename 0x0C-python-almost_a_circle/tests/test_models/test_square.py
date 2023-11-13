68d321d4c172055da22b66834827e2f6a6ad2f2d
josephorokpo <josephorokpo@gmail.com>
2023-11-13 02:00:47 -0800

    test_square.py with the public method update

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -2,27 +2,62 @@ import unittest
 from models.square import Square
 
 
-class TestSquareSizeGetterSetter(unittest.TestCase):
+class TestSquareUpdateMethod(unittest.TestCase):
     """
-    Test cases for the size getter and setter of the Square class.
+    Test cases for the update method of the Square class.
     """
 
-    def test_size_setter(self):
+    def test_update_method_no_args(self):
         """
-        Test if the size setter works as expected.
+        Test if update method does not modify attributes when
+        no arguments are provided.
         """
         square = Square(5, 2, 3, 1)
+        square.update()
 
-        square.size = 8
+        self.assertEqual(square.id, 1)
+        self.assertEqual(square.size, 5)
+        self.assertEqual(square.x, 2)
+        self.assertEqual(square.y, 3)
+
+    def test_update_method_with_args(self):
+        """
+        Test if update method correctly updates attributes
+        with provided arguments.
+        """
+        square = Square(5, 2, 3, 1)
+        square.update(2, 8, 4, 6)
+
+        self.assertEqual(square.id, 2)
         self.assertEqual(square.size, 8)
-        self.assertEqual(square.width, 8)
-        self.assertEqual(square.height, 8)
+        self.assertEqual(square.x, 4)
+        self.assertEqual(square.y, 6)
 
-        with self.assertRaises(TypeError):
-            square.size = "invalid_size"
+    def test_update_method_partial_args(self):
+        """
+        Test if update method correctly updates attributes
+        with partial arguments.
+        """
+        square = Square(5, 2, 3, 1)
+        square.update(2, 8)
 
-        with self.assertRaises(ValueError):
-            square.size = 0
+        self.assertEqual(square.id, 2)
+        self.assertEqual(square.size, 8)
+        self.assertEqual(square.x, 2)
+        self.assertEqual(square.y, 3)
+
+    def test_update_method_keyword_args(self):
+        """
+        Test if update method correctly updates attributes
+        with keyword arguments.
+        """
+        square = Square(5, 2, 3, 1)
+        square.update(id=2, size=8, x=4, y=6)
+
+        self.assertEqual(square.id, 2)
+        self.assertEqual(square.size, 8)
+        self.assertEqual(square.x, 4)
+        self.assertEqual(square.y, 6)
 
 
 if __name__ == '__main__':
