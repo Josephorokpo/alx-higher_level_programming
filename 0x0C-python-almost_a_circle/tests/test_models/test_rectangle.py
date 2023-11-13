809ad8c3919e3879d2acec0bb4624bf54315331e
josephorokpo <josephorokpo@gmail.com>
2023-11-13 04:18:40 -0800

    test_rectangle.py with  the public method to_dictionary

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,80 +2,39 @@ import unittest
 from models.rectangle import Rectangle
 
 
-class TestRectangleUpdateMethod(unittest.TestCase):
-    """
-    Test cases for the update method of the Rectangle class.
-    """
+class TestRectangleMethods(unittest.TestCase):
+    def test_area(self):
+        rectangle = Rectangle(5, 10)
+        self.assertEqual(rectangle.area(), 50)
 
-    def test_update_method_no_args(self):
-        """
-        Test if update method does not modify attributes when
-        no arguments are provided.
-        """
-        rectangle = Rectangle(10, 20, 5, 8, 1)
-        rectangle.update()
+    def test_display(self):
+        rectangle = Rectangle(3, 2)
+        expected_output = "###\n###\n"
+        with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            rectangle.display()
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-        self.assertEqual(rectangle.id, 1)
-        self.assertEqual(rectangle.width, 10)
-        self.assertEqual(rectangle.height, 20)
-        self.assertEqual(rectangle.x, 5)
-        self.assertEqual(rectangle.y, 8)
+    def test_str_representation(self):
+        rectangle = Rectangle(4, 8, 2, 3, 7)
+        expected_str = "[Rectangle] (7) 2/3 - 4/8"
+        self.assertEqual(str(rectangle), expected_str)
 
     def test_update_method_with_args(self):
-        """
-        Test if update method correctly updates attributes
-        with provided arguments.
-        """
         rectangle = Rectangle(10, 20, 5, 8, 1)
         rectangle.update(2, 15, 25, 3, 4)
-
-        self.assertEqual(rectangle.id, 2)
-        self.assertEqual(rectangle.width, 15)
-        self.assertEqual(rectangle.height, 25)
-        self.assertEqual(rectangle.x, 3)
-        self.assertEqual(rectangle.y, 4)
+        expected_str = "[Rectangle] (2) 3/4 - 15/25"
+        self.assertEqual(str(rectangle), expected_str)
 
     def test_update_method_partial_args(self):
-        """
-        Test if update method correctly updates attributes with
-        partial arguments.
-        """
         rectangle = Rectangle(10, 20, 5, 8, 1)
         rectangle.update(2, 15)
+        expected_str = "[Rectangle] (2) 5/8 - 15/20"
+        self.assertEqual(str(rectangle), expected_str)
 
-        self.assertEqual(rectangle.id, 2)
-        self.assertEqual(rectangle.width, 15)
-        self.assertEqual(rectangle.height, 20)
-        self.assertEqual(rectangle.x, 5)
-        self.assertEqual(rectangle.y, 8)
-
-    def test_update_method_extra_args(self):
-        """
-        Test if update method ignores extra arguments beyond
-        the fifth argument.
-        """
+    def test_to_dictionary(self):
         rectangle = Rectangle(10, 20, 5, 8, 1)
-        rectangle.update(2, 15, 25, 3, 4, 99, "extra")
-
-        self.assertEqual(rectangle.id, 2)
-        self.assertEqual(rectangle.width, 15)
-        self.assertEqual(rectangle.height, 25)
-        self.assertEqual(rectangle.x, 3)
-        self.assertEqual(rectangle.y, 4)
-
-    def test_update_method_keyword_args(self):
-        """
-        Test if update method correctly updates attributes with
-        keyword arguments.
-        """
-        rectangle = Rectangle(10, 20, 5, 8, 1)
-        rectangle.update(id=2, width=15, height=25, x=3, y=4)
-
-        self.assertEqual(rectangle.id, 2)
-        self.assertEqual(rectangle.width, 15)
-        self.assertEqual(rectangle.height, 25)
-        self.assertEqual(rectangle.x, 3)
-        self.assertEqual(rectangle.y, 4)
+        expected_dict = {'id': 1, 'width': 10, 'height': 20, 'x': 5, 'y': 8}
+        self.assertEqual(rectangle.to_dictionary(), expected_dict)
 
 
 if __name__ == '__main__':
