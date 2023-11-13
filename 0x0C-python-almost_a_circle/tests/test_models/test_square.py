3ad0390a1b4500c838e43fc7243cbeae0404ddb7
josephorokpo <josephorokpo@gmail.com>
2023-11-13 05:55:25 -0800

    test_square.py with added method to_dictionary

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -2,15 +2,24 @@ import unittest
 from models.square import Square
 
 
-class TestSquareUpdateMethod(unittest.TestCase):
+class TestSquareMethods(unittest.TestCase):
     """
-    Test cases for the update method of the Square class.
+    Test cases for the Square class.
     """
+
+    def test_to_dictionary(self):
+        """
+        Test the to_dictionary method of the Square class.
+        """
+        square = Square(5, 2, 3, 1)
+        dictionary = square.to_dictionary()
+
+        self.assertEqual(dictionary, {'id': 1, 'size': 5, 'x': 2, 'y': 3})
 
     def test_update_method_no_args(self):
         """
-        Test if update method does not modify attributes
-        when no arguments are provided.
+        Test if update method does not modify attributes when no
+        arguments are provided.
         """
         square = Square(5, 2, 3, 1)
         square.update()
@@ -26,12 +35,12 @@ class TestSquareUpdateMethod(unittest.TestCase):
         with provided arguments.
         """
         square = Square(5, 2, 3, 1)
-        square.update(2, 8, 4, 6)
+        square.update(2, 8, 4, 7)
 
         self.assertEqual(square.id, 2)
         self.assertEqual(square.size, 8)
         self.assertEqual(square.x, 4)
-        self.assertEqual(square.y, 6)
+        self.assertEqual(square.y, 7)
 
     def test_update_method_partial_args(self):
         """
@@ -48,16 +57,16 @@ class TestSquareUpdateMethod(unittest.TestCase):
 
     def test_update_method_extra_args(self):
         """
-        Test if update method ignores extra arguments
-        beyond the fourth argument.
+        Test if update method ignores extra arguments beyond
+        the fourth argument.
         """
         square = Square(5, 2, 3, 1)
-        square.update(2, 8, 4, 6, 99, "extra")
+        square.update(2, 8, 4, 7, 99, "extra")
 
         self.assertEqual(square.id, 2)
         self.assertEqual(square.size, 8)
         self.assertEqual(square.x, 4)
-        self.assertEqual(square.y, 6)
+        self.assertEqual(square.y, 7)
 
 
 if __name__ == '__main__':
