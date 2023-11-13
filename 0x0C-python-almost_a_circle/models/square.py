70da8a2986dbfd9376119ce6cfbe08b299652df5
josephorokpo <josephorokpo@gmail.com>
2023-11-13 01:26:16 -0800

    square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Square class that inherits from Rectangle.
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Constructor for the Square class.
+
+        :param size: Size of the square.
+        :param x: X-coordinate of the square (default is 0).
+        :param y: Y-coordinate of the square (default is 0).
+        :param id: ID of the square (default is None).
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter for size attribute."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size attribute."""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Return a string representation of the square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
