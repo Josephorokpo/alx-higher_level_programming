18793ffdd23b8b71aa91a60dd6eb1cb3148d91b0
josephorokpo <josephorokpo@gmail.com>
2023-12-12 13:37:03 -0800

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,7 @@
---List all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+
+-- List all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 
 SELECT score, name
 FROM second_table
-WHERE name != ""
+WHERE name != ''
 ORDER BY score DESC;
