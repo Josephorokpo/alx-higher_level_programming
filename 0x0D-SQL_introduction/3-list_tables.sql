f673a135d172efa991524e52a5054eaab07d4ef7
josephorokpo <josephorokpo@gmail.com>
2023-12-12 06:28:39 -0800

    3-list_tables.sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,7 +1,4 @@
 -- Task: List all tables in a specified MySQL database.
 
--- Use the specified database passed as an argument.
-USE mysql;
-
 -- List all tables in the selected database.
 SHOW TABLES;
