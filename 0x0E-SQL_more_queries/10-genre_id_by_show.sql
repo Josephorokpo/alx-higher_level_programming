ab65911b3bc9222ef8fdc4f82e3475209867c547
josephorokpo <josephorokpo@gmail.com>
2023-12-13 13:49:32 -0800

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 
 -- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM your_database_name.tv_shows
-JOIN your_database_name.tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+FROM hbtn_0d_tvshows.tv_shows
+JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
