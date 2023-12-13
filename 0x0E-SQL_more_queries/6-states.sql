de833743a191992eb701493cc674969911d0879a
josephorokpo <josephorokpo@gmail.com>
2023-12-13 12:44:56 -0800

    6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Create the database hbtn_0d_usa if it does not exist
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+ 
+-- Create the table states in the database hbtn_0d_usa on your MySQL server
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    `id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `name` VARCHAR(256) NOT NULL
+);
