f56b786897ca31fae2467ce292be14801cb8bcd3
josephorokpo <josephorokpo@gmail.com>
2023-12-14 02:55:30 -0800

    14-my_genres.sql

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Use the hbtn_0d_tvshows database to list all genres of the show Dexter.
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+SELECT g.`name`
+FROM `tv_genres` AS g
+INNER JOIN `tv_show_genres` AS s ON g.`id` = s.`genre_id`
+INNER JOIN `tv_shows` AS t ON t.`id` = s.`show_id` AND t.`title` = 'Dexter'
+ORDER BY g.`name`;
