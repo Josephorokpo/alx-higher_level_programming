3154c4bef13b703bae6c1ee428899f858abc4f1f
josephorokpo <josephorokpo@gmail.com>
2024-01-09 07:04:32 -0800

    10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,13 +1,2 @@
 #!/usr/bin/node
-
-exports.converter = function (base) {
-  // Use a self-invoking function to avoid declaring new variables
-  (function convert(num) {
-    // If num is greater than or equal to base, continue converting
-    num >= base && convert((num - num % base) / base);
-
-    // Output the remainder after division as the converted value
-    process.stdout.write(num % base < 10 ? num % base + '' : String.fromCharCode(num % base - 10 + 'a'.charCodeAt(0)));
-  });
-  process.stdout.write('\n'); // Print a new line after conversion
-};
+exports.converter = function (base) { return num => num.toString(base); };
