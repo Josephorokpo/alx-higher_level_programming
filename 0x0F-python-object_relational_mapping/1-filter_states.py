c3554fb61111b7dd1130373703a27f4be65e52f2
josephorokpo <josephorokpo@gmail.com>
2024-01-12 06:05:58 -0800

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,10 +8,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if all three arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
 
     # Connect to MySQL server
     db = MySQLdb.connect(
