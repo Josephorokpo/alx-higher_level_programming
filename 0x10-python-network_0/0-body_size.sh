85670ee0e857870969db39c64fecd7a958153061
josephorokpo <josephorokpo@gmail.com>
2024-01-25 05:05:10 -0800

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 # displays the size of the response body in bytes in a URL using curl 
 
-curl -s "$1" | wc -c
+# Check if URL argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send request using curl in silent mode (-s) and get the size of the response body in bytes
+response_size=$(curl -s -w "%{size_download}" -o /dev/null "$1")
+
+# Display the size of the response body
+echo "Size of the response body: $response_size bytes"
