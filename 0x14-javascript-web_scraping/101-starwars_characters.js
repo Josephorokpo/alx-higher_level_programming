7d0c4a27f95e2404fe11732d0b638616e55ae07a
josephorokpo <josephorokpo@gmail.com>
2024-02-20 05:24:07 -0800

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,20 +3,23 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(apiUrl, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const movieData = JSON.parse(body);
-    const charactersUrls = movieData.characters;
-
-    charactersUrls.forEach(characterUrl => {
-      request(characterUrl, function (err, resp, body) {
-        if (!err && resp.statusCode === 200) {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        }
-      });
-    });
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters(characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+  request(characters[index], function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log(JSON.parse(body).name);
+      printCharacters(characters, index + 1);
+    }
+  });
+}
